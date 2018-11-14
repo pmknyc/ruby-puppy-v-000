5a04670e98755f4969f2c54d3a01447b9fe81fec
Paula McKinley <pmknyc@optimum.net>
2018-11-14 18:21:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,23 +4,15 @@ class Dog
 
   attr_accessor :name
   @@all = []
-	@@names = []
 
   def initialize(name)
     @name = name
     @@all << self
-		@@names << name
   end
 
   def self.all
-    @@all.each do |name|
-      puts "#{self.name}"
-    end
-  end
-
-  def self.name_all
-    @@names.each do |name|
-      puts "#{name}"
+    @@all.each do |dog|
+      puts "#{dog.name}"
     end
   end
 
